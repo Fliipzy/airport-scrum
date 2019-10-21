60985f877c141ebc58194010ab3cdb62f1d6d0d0
Frede <Frederiklundbeck@live.dk>
2019-10-21 10:47:11 +0200

    initial commit

---
+++ lufthansa-console/src/main/java/edu/dat18c/lufthansa/App.java
@@ -0,0 +1,13 @@
+package edu.dat18c.lufthansa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
