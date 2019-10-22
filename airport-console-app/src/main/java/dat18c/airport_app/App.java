5eba8b97e5aba1f3f75909bee9c9aa84550beeb4
Wajiddat18c <waji0003@stud.kea.dk>
2019-10-22 14:33:46 +0200

    Update App.java

--- airport-console-app/src/main/java/dat18c/airport_app/App.java
+++ airport-console-app/src/main/java/dat18c/airport_app/App.java
@@ -13,25 +13,6 @@ public class App
 {
     public static void main( String[] args )
     {
-
-        /*try
-        {
-            var dbConnection = DatabaseConnection.getInstance();
-            dbConnection.connect();
-            var statement = dbConnection.getConnection().createStatement();
-            var resultSet = statement.executeQuery("SELECT * FROM flyselskab");
-
-            int i = 0;
-            if (resultSet.next()) 
-            {
-                System.out.println(resultSet.getString(2));
-            }
-        } 
-        catch (Exception e) 
-        {
-            System.out.println(e.toString());
-            //TODO: handle exception
-        }*/
         
 
         IDatabaseConnection dbConnection = DatabaseConnection.getInstance();
