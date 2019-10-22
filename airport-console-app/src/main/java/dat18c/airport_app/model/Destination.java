c7bfab867a6d911e27b3a3b4df1315f0df7b32cc
Ali Al sharefi <alix4970@stud.kea.dk>
2019-10-22 13:18:05 +0200

    Getters og setter mangler

---
+++ airport-console-app/src/main/java/dat18c/airport_app/model/Destination.java
@@ -0,0 +1,7 @@
+package dat18c.airport_app.model;
+
+public class Destination {
+    private int idDestination;
+    private String fra_land;
+    private String ankomst_tidspunkt;
+}
---
+++ airport-console-app/src/main/java/dat18c/airport_app/model/ServiceSelskab.java
@@ -0,0 +1,17 @@
+package dat18c.airport_app.model;
+
+public class ServiceSelskab {
+    private int idServiceSelskab;
+    private String size;
+    private int Taxi_ind_ved_ankomst;
+    private int Passagerer_ud;
+    private int Bagage_ud;
+    private int Brændstof_påfyldning;
+    private int Rengøring;
+    private int Bagage_ind;
+    private int Passagere_ind;
+    private int Taxi_ud_til_afgang;
+    private int Taxi_til_og_ofra_ventepladser;
+    private int Personale_til_og_fra_øvrige_i_egen_terminal;
+    private int Personale_til_og_fra_den_anden_terminal;
+}
