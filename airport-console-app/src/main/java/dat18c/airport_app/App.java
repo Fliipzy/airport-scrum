c4558b0e35a770b7e7e83147d18054910571363d
Frede <Frederiklundbeck@live.dk>
2019-10-22 14:06:42 +0200

    added some test code in App.java class

--- airport-console-app/src/main/java/dat18c/airport_app/App.java
+++ airport-console-app/src/main/java/dat18c/airport_app/App.java
@@ -1,18 +1,32 @@
 package dat18c.airport_app;
 
 import dat18c.airport_app.db.DatabaseConnection;
-import dat18c.airport_app.db.interfaces.IDatabaseConnection;
 
 /**
  * Hello world!
- *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        IDatabaseConnection dbConnection = DatabaseConnection.getInstance();
-        dbConnection.connect();
-        System.out.println("WINNER WINNER CHICKEN DINNER!");
+        try 
+        {
+            var dbConnection = DatabaseConnection.getInstance();
+            dbConnection.connect();
+            var statement = dbConnection.getConnection().createStatement();
+            var resultSet = statement.executeQuery("SELECT * FROM flyselskab");
+
+            int i = 0;
+            if (resultSet.next()) 
+            {
+                System.out.println(resultSet.getString(2));
+            }
+        } 
+        catch (Exception e) 
+        {
+            System.out.println(e.toString());
+            //TODO: handle exception
+        }
+        
     }
 }
