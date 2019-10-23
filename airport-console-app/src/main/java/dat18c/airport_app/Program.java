a46dedd237c5b02a99a261ee4406e2ca6e1e655d
Ali Al sharefi <alix4970@stud.kea.dk>
2019-10-23 10:25:54 +0200

    Menu - er der mangler?

--- airport-console-app/src/main/java/dat18c/airport_app/Program.java
+++ airport-console-app/src/main/java/dat18c/airport_app/Program.java
@@ -1,5 +1,7 @@
 package dat18c.airport_app;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
@@ -7,6 +9,32 @@ public class Program
 {
     public static void main(String[] args) 
     {
-        
+        Scanner sc = new Scanner(System.in);
+
+        //Display Menu
+        System.out.println("1\t Flight information");
+        System.out.println("2\t Arrivals and Depatures");
+        System.out.println("3\t Airplane Service");
+        System.out.println("4\t Apron");
+        System.out.println("5\t ");
+
+        System.out.println("Please enter your choice");
+
+        //Getting user input
+        int choice= sc.nextInt();
+
+
+        switch (choice){
+            case 1:
+                System.out.println("Flight Information");
+            case 2:
+                System.out.println("Arrivals and Depatures");
+            case 3:
+                System.out.println("Airplane Service");
+            case 4:
+                System.out.println("Apron");
+            case 5:
+                System.out.println("----");
+        }
     }
 }
